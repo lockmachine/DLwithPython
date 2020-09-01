937b81745a657bff5a4ac1d4c2b5a8e6c5a0b70f
lockmachine <lock_machine@yahoo.co.jp>
2020-09-01 16:22:32 +0900

    fix vgg16base

--- chapter5/3-1_vgg16base.py
+++ chapter5/3-1_vgg16base.py
@@ -2,6 +2,11 @@ from keras.applications import VGG16
 import os
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+from keras import models
+from keras import layers
+from keras import optimizers
+import matplotlib.pyplot as plt
 
 conv_base = VGG16(
                     weights='imagenet',
@@ -15,7 +20,7 @@ conv_base = VGG16(
 base_dir = '/home/makoto/work/shake/DLwithPython/chapter5/dataset_small'
 
 train_dir = os.path.join(base_dir, 'train')
-validaton_dir = os.path.join(base_dir, 'validation')
+validation_dir = os.path.join(base_dir, 'validation')
 test_dir = os.path.join(base_dir, 'test')
 
 datagen = ImageDataGenerator(rescale=1./255)
@@ -31,7 +36,7 @@ def extract_features(directory, sample_count):
                                             class_mode='binary'
                                             )
     i = 0
-    for inputs_batch, labels in generator:
+    for inputs_batch, labels_batch in generator:
         features_batch = conv_base.predict(inputs_batch)
         features[i * batch_size : (i+1) * batch_size] = features_batch
         labels[i * batch_size : (i+1) * batch_size] = labels_batch
@@ -41,4 +46,49 @@ def extract_features(directory, sample_count):
             break
     return features, labels
 
-    
+train_features, train_labels = extract_features(train_dir, 2000)
+validation_features, validation_labels = extract_features(validation_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+train_features = np.reshape(train_features, (2000, 4*4*512))
+validation_features = np.reshape(validation_features, (1000, 4*4*512))
+test_features = np.reshape(test_features, (1000, 4*4*512))
+
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=4*4*512))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5), loss='binary_crossentropy', metrics=['acc'])
+
+history = model.fit(
+                    train_features,
+                    train_labels,
+                    epochs=30,
+                    batch_size=20,
+                    validation_data=(validation_features, validation_labels)
+                    )
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+# 正解率をプロット
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+
+# 損失値をプロット
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
